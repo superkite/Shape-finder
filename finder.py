1f93c8713d93db2d6eee0fc5a812cac3d73466e6
superkite <superkite395@gmail.com>
2017-09-27 11:33:06 -0400

    v1

---
+++ finder.py
@@ -0,0 +1,22 @@
+from math import *
+x1 =
+y1 =
+
+x2 =
+y2 =
+
+x3 =
+y3 =
+
+x4 =
+y4 =
+
+mAB = (y2 - y1)/(x2 - x1)
+mBC = (y3 - y2)/(x3 - x2)
+mCD = (y4 - y3)/(x4 - x3)
+mDA = (y1 - y4)/(x1 - x4)
+
+lAB = sqrt((x2 - x1)**2 - (y2 - y1)**2)
+lBC = sqrt((x3 - x2)**2 - (y3 - y2)**2)
+lCD = sqrt((x4 - x3)**2 - (y4 - y3)**2)
+lDA = sqrt((x1 - x4)**2 - (y1 - y4)**2)
